603b241be8b6d396062135fcf4b31e82853c4f48
Moshe Baram <>
2021-11-25 01:03:06 +0100

    Fixed wrong layout reference

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Nav from "../components/nav";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 
 const Home = () => {
   return (
